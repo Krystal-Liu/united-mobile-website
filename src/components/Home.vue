0522e9656843943f8273c7eb19c397d612e02ebe
Krystal <krystal@un-net.com>
2018-08-16 15:29:55 +0800

    second commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="home">
+        <p>Home</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    #home {
+        p {
+            font-size: 60px;
+        }
+    }
+</style>
